cb371ce6998cda44eee502b42c68c45f2536b799
James Rose <james.rose1103@protonmail.com>
2024-06-20 10:38:52 -0400

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+apt-get install openjdk-21-jre
+apt-get install gradle
+apt-get install rustup
+apt-get install npm
+npm install --force -g npm@latest
+npm install --force -g nodejs@latest
+
+mkdir Android
+wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+unzip *.zip -d Android
+
+cd Android/cmdline-tools
+yes | bin/sdkmanager --licenses --sdk_root=..> /dev/null || true
+
+# Install SDK Platform
+bin/sdkmanager --sdk_root=.. "platforms;android-33"
+# Install Platform Tools
+bin/sdkmanager --sdk_root=.. "platform-tools"
+# Install Build Tools
+bin/sdkmanager --sdk_root=.. "build-tools;34.0.0"
+# Install NDK
+bin/sdkmanager --sdk_root=.. "ndk;27.0.11718014"
